c5925ad0291e49850c25a4c8ed2bddd30df843de
Metin Emenullahi <metin@mtndesign.net>
2013-05-15 21:51:11 +0300

    Indentation errors fixed in Burak's code.

--- src/ga_bases/GAUserManBase.py
+++ src/ga_bases/GAUserManBase.py
@@ -6,26 +6,25 @@ class GAUserMan(GABase):
         super(GABase,self).__init__()
         self.authentication = auth
 
-    def add(self, usr, grps = None):
+    def add(self, usr, grps=None):
         gservice=self.authentication.gservice
-	listOfEmpties = ["",[],{},None]
-	if isinstance(grps,basestring):
-	    self.adding_result = self.add_user_to_group(gservice, 
-							usr, 
-							grps)
-
-        elif isinstance(grps,list):
+        listOfEmpties = ["", [], {}, None]
+        if isinstance(grps, basestring):
+            self.adding_result = self.add_user_to_group(gservice, 
+                                                        usr, 
+                                                        grps)
+        elif isinstance(grps, list):
             for grp in grps:
                 self.adding_result = self.add_user_to_group(gservice,
                                                             usr, 
                                                             grp)
                 print self.adding_result
-
         elif grps in listOfEmpties:
             self.all_groups = self.get_all_groups(gservice)
             for grp in self.all_groups:
-                self.adding_result = self.add_user_to_group(gservice, usr, grp)
+                self.adding_result = self.add_user_to_group(gservice, 
+                                                            usr, 
+                                                            grp)
                 print self.adding_result
-
         else:
-	    print "Please provide valid group(s)\n"
+            print "Please provide valid group(s)\n"
